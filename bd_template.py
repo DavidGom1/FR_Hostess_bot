9324e4d9243402dd8d1b68b4e41371de6e29dca5
root <david.gomez4@um.es>
2024-03-10 17:40:21 +0000

    Actualizacion

---
+++ bd_template.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+sistema = ["Digi", "Anal"]
+dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+frecuencias_dias = ["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8"]
+days_translate = {"Monday": "Lunes","Tuesday": "Martes","Wednesday": "Miércoles","Thursday": "Jueves","Friday": "Viernes","Saturday": "Sábado","Sunday": "Domingo"}
+
+config = {
+    'host' : 'localhost',
+    'port' : '3306',
+    'user' : 'root',
+    'password' : '6778'
+}
